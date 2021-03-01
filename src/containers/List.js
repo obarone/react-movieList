e7dd24b520b2c6434421ebd00d050511524e3c60
Andrea Barone <ndrbarone@gmail.com>
2021-03-01 17:54:44 +0100

    first commit

---
+++ src/containers/List.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Card from '../components/Card';
+
+class List extends Component {
+  constructor () {
+    super();
+    this.state = {
+      data: [],
+      loading: true
+    };
+  }
+
+  async componentDidMount () {
+    const movies = await fetch('../assets/data.json');
+    const moviesJSON = await movies.json();
+
+    if (moviesJSON) {
+      this.setState({
+        data: moviesJSON,
+        loading: false
+      });
+    }
+  }
+
+  render () {
+    const { data, loading } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    return data.map(movie => <Card key={movie.id} movie={ movie } />);
+  }
+}
+
+export default List;
